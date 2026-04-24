bd880443adacd52c487cda6065b485a270cf27c6
Eric Lowry <ericlowry14@gmail.com>
2026-04-24 14:46:17 +0200

    Proper version code update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.ericlowry.dnstoggle"
 		minSdk = 28
 		targetSdk = 36
-		versionCode = 1
-		versionName = "1.0.1"
+		versionCode = 2
+		versionName = "1.0.2"
 	}
 
 	buildTypes {
