a3e26baa3c40e449c70a36a9406cc99cec40c713
Eric Lowry <ericlowry14@gmail.com>
2026-04-24 15:41:16 +0200

    Moved fastlane metadata folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.ericlowry.dnstoggle"
 		minSdk = 28
 		targetSdk = 36
-		versionCode = 2
-		versionName = "1.0.2"
+		versionCode = 3
+		versionName = "1.0.3"
 	}
 
 	buildTypes {
